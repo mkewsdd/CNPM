957e664899e4239e4610397340fd4550bef113a6
unknown <huynguyen20041119007@gmail.com>
2023-11-30 16:52:01 +0700

    upload

--- milkteaweb/login/login.js
+++ milkteaweb/login/login.js
@@ -49,43 +49,73 @@ function RegisterHandle() {
 }
 RegisterHandle();
 
-function forgetPasswordHandle() {
-    const fpBtn = $('.forget_password_container');
+function forgetPasswordHandle() {    
     const fpForm = $('.form_fp');
+    const fpBtn = $('.forget_password_container');
     const closeFpForm = $('.close_form_fp');
     const checkEmail = $('.check_email_container');
+    const emailInput = $('#emailInput');
+    const verificationAnswers = $$('input[name="verificationAnswer"]');
+    const searchButton = $('#searchButton');
+    const verifyButton = $('#verifyButton');
+    const notification = $('.password_reset_notification');
+    const closeNotification = $('.password_reset_notification_close_btn');
 
     fpBtn.addEventListener('click', function(){
         fpForm.style.display = 'flex';
-    })
+    });
 
     closeFpForm.addEventListener('click', function(){
+        clearForm();
         fpForm.style.display = 'none';
     });
 
-    checkEmail.addEventListener('click', function(){
-        fpForm.style.display = 'none';
-    });
+    verifyButton.addEventListener('click', verifyAnswer);
+
+    function clearForm() {
+        emailInput.value = ''; // Clear the email input
+
+        verificationAnswers.forEach(answer => {
+            answer.checked = false; // Uncheck all radio buttons
+        });
+
+        checkEmail.style.display = 'none';
+        searchButton.disabled = true;
+    }
+
+    function verifyAnswer() {
+        var selectedAnswer = document.querySelector('input[name="verificationAnswer"]:checked');
+        var emailInputValue = emailInput.value;
+
+        if (selectedAnswer && selectedAnswer.value === "9" && emailInputValue) {
+            checkEmail.style.display = 'block';
+            searchButton.disabled = false;
+        } else if (selectedAnswer && !emailInputValue) {
+            alert('Please enter your email address.');
+        } else if (!selectedAnswer && emailInputValue) {
+            alert('Please answer the verification question.');
+        } else {
+            alert('Your verification answer is wrong please answer again.');
+        }
+    }
+
+    function notificationFormHandle() {
+        checkEmail.addEventListener('click', function(e) {
+            e.preventDefault();
+            if(notification.style.display = 'none'){
+                clearForm();
+                notification.style.display = 'flex';
+                fpForm.style.display = 'none';
+            }
+        })
+
+        closeNotification.addEventListener('click', function(e) {
+            if(notification.style.display = 'flex'){
+                notification.style.display = 'none';
+            }
+        })
+    }
+    notificationFormHandle();
 
 }
 forgetPasswordHandle();
-
-function verifyAnswer() {
-    var selectedAnswer = document.querySelector('input[name="verificationAnswer"]:checked');
-    if (selectedAnswer && selectedAnswer.value === "9") {
-        document.querySelector('.check_email_container').style.display = 'block';
-        document.getElementById('searchButton').disabled = false;
-    } else if (selectedAnswer) {
-        alert('Incorrect answer. Please try again.');
-    } else {
-        // User has not selected an answer yet
-        // You can choose to display a message or take other actions
-    }
-}
-
-document.getElementById('forgotPasswordForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    verifyAnswer(); // Call the verifyAnswer function on form submission
-});
-
-verifyAnswer();
